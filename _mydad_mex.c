8c8fc111e7038d4296f7562be0c5297e816ca4b7
Cynthia John Echemeozor <120315188+CynthiaJohn11@users.noreply.github.com>
2023-10-17 06:04:51 +0100

    Update _mydad_mex.c

--- _mydad_mex.c
+++ _mydad_mex.c
@@ -8,29 +8,14 @@
 
 int dad_mex(va_list val)
 {
-	int g, jump = 0;
-	int *array;
-	unsigned int min = va_arg(val, unsigned int);
-	unsigned int cem = min;
+	int jump;
+	char *cynstr;
 
-	while (min / 16 != 0)
-	{
-		min = min / 16;
-		jump++;
-	}
-/*	jump++;*/
-	array = malloc(sizeof(int) * jump);
-	for (g = 0; g < jump; g++)
-	{
-		array[g] = cem % 16;
-		cem = cem / 16;
-	}
-	for (g = jump - 1; g >= 0 ; g++)
-	{
-		if (array[g] > 9)
-		array[g] = array[g] + 39;
-		_putchar(array[g] + '0');
-	}
-	free(array);
+	cynstr = itao(va_arg(args, unsigned int), 16);
+
+	s = cynputs((cynstr != NULL) ? cynstr : "NULL");
+
+	return (s);
+
 	return (jump);
 }
