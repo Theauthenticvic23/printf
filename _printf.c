96aa2bca8cf4632454f7a7082327c194c85bbbb7
Cynthia John Echemeozor <120315188+CynthiaJohn11@users.noreply.github.com>
2023-10-17 05:09:38 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,6 +1,42 @@
 #include "main.h"
 
 /**
+ * flag_handler - Controller for percent format
+ * @s: String format
+ * @args: List of arguments
+ * @i: Iterator
+ * Return: Size of the numbers of elements printed
+ **/
+
+int flag_handler(const char *s, va_list args, int *i)
+{
+	int len = 0, j, num_f;
+
+	match m[] = {
+	{"c", customp_char}, {"s", customp_string}, {"%", custom_percent}, {"d", vic_dec}, {"i", vic_int}, {"r", print_revs}, {"R", print_rot13}, {"b", mum_bin}, {"u", vic_unsigned}, {"o", mum_octal}, {"x", dad_mex}, {"X", print_Hex}, {"p", myini_pointer}
+	};
+
+	*i++;
+	if (s[*i] == '\0')
+		return (-1);
+
+	num_f = sizeof(m) / sizeof(m[0]);
+	for (j = 0; j < num_f; j++)
+	{
+		if (s[*i] == m[j].id)
+		{
+			len = m[j].f(args);
+			return (len);
+			
+			continue;
+		}
+	}
+	_putchar('%'), _putchar(str[*i]);
+
+	return (2);
+}
+
+/**
  * _printf - first printf
  * @format: character
  * Return: i
@@ -8,13 +44,10 @@
 
 int _printf(const char *format, ...)
 {
-	match m[] = {
-	{"c", customp_char}, {"s", customp_string}, {"%", custom_percent}, {"d", vic_dec}, {"i", vic_int}, {"r", print_revs}, {"R", print_rot13}, {"b", mum_bin}, {"u", vic_unsigned}, {"o", mum_octal}, {"x", dad_mex}, {"X", print_Hex}, {"p", myini_pointer}
-	};
+	
 
 	va_list args;
 	int i = 0, len = 0, num_f;
-	int j;
 
 	va_start(args, format);
 
@@ -27,30 +60,21 @@ int _printf(const char *format, ...)
 			if ('%' == format[i])
 			{
 				c_buffer(-1);
-				i++;
-				if (format[i] == '\0')
+				cyn = flag_handler(format, args, &i);
+				if (cyn == -1)
 					return (-1);
-				num_f = sizeof(m) / sizeof(m[0]);
-				for (j = 0; j < num_f; j++)
-				{
-					if (format[i] == m[j].id)
-					{
-						len = m[j].f(args);
-						i++;
-						j++;
-						continue;
-					}
-				}
+				len += cyn;
+				continue;
 			}
-		
-	
-		}
-	c_buffer(format[i]);
+			else
+			{
+				c_buffer(format[i]);
 /*			_putchar(format[i]);*/
-			i++;
-			len++;
+			}
+		i++;
+		len++;
 	}
-c_buffer(-1);
+	c_buffer(-1);
 	va_end(args);
 	return (len);
 }
