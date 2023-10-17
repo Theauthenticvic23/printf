d494c9a1092711ac6f6dd477e0ea7d710f517a2f
Cynthia John Echemeozor <120315188+CynthiaJohn11@users.noreply.github.com>
2023-10-17 06:15:38 +0100

    Update _mysis_upper.c

--- _mysis_upper.c
+++ _mysis_upper.c
@@ -1,6 +1,17 @@
 #include "main.h"
 
 /**
+ * _islower - checks for lowercase character
+ * @cy: the character for checking
+ * Return: 1 if c is lowercase success, 0 otherwise or error
+ */
+int _islower(int cy)
+{
+	return (cy >= 'a' && cy <= 'z');
+}
+
+
+/**
  * stou - Change the string to uppercase
  * @cyn: String
  * Return: String uppercase
