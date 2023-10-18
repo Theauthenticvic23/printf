d42600d22fe3416f01a1bab0563a14a791142083
Cynthia John Echemeozor <ugochijohn90@gmail.com>
2023-10-18 06:55:27 +0000

    now

--- _cynthia.c
+++ _cynthia.c
@@ -1,63 +1,140 @@
 #include "main.h"
 
 /**
- * c_buffer - Save the character in a buffer
- * @t: Character
- * Return: 1
- **/
-
-int c_buffer(char t)
+ * cyn_flags - Calculates active flags
+ * @fstr: Formatted string in which to print the arguments
+ * @i: take a parameter.
+ * Return: Flags:
+ */
+int cyn_flags(const char *fstr, int *i)
 {
-	static char buff[1024];
-	static int j;
+	/* - + 0 # ' ' */
+	/* 1 2 4 8  16 */
+	int j, curr_i;
+	int flags = 0;
+	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLAGS_ARR[] = {C_MINUS, C_PLUS, C_ZERO, C_HASH, C_SPACE, 0};
 
-	if (t == -1 || j == 1024)
+	for (curr_i = *i + 1; fstr[curr_i] != '\0'; curr_i++)
 	{
-		write(1, buff, j);
-		j = 0;
+		for (j = 0; FLAGS_CH[j] != '\0'; j++)
+			if (fstr[curr_i] == FLAGS_CH[j])
+			{
+				flags |= FLAGS_ARR[j];
+				break;
+			}
+
+		if (FLAGS_CH[j] == 0)
+			break;
 	}
 
-	if (t != -1)
-		buff[j++] = t;
+	*i = curr_i - 1;
 
-	return (1);
+	return (flags);
 }
 
+#include "main.h"
+
 /**
- * print_cyn - string
- * @val: args
- * Return: nothing
+ * cyn_precision - Calculates the precision for printing
+ * @fstr: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
+ * @val: list of arguments.
+ *
+ * Return: Precision.
  */
-
-int print_cyn(va_list val)
+int cyn_precision(const char *fstr, int *i, va_list val)
 {
-	char *s;
-	int j, length = 0;
-	int value;
+	int curr_i = *i + 1;
+	int precision = -1;
 
-	s = va_arg(val, char *);
-	if (s == NULL)
-		s = "(null)";
-	for (j = 0; s[j] != '\0'; j++)
+	if (fstr[curr_i] != '.')
+		return (precision);
+
+	precision = 0;
+
+	for (curr_i += 1; fstr[curr_i] != '\0'; curr_i++)
 	{
-		if (s[j] < 32 || s[j] >= 127)
+		if (is_digit(fstr[curr_i]))
 		{
-			_putchar('\\');
-			_putchar('x');
-			length = length + 2;
-			value = s[j];
-			if (value < 16)
-			{
-				_putchar('0');
-				length++;
-			}
-			length  = length + print_extra(value);
+			precision *= 10;
+			precision += fstr[curr_i] - '0';
+		}
+		else if (fstr[curr_i] == '*')
+		{
+			curr_i++;
+			precision = va_arg(val, int);
+			break;
 		}
 		else
-		{
-			_putchar(s[j]);
-			length++;
-		}
+			break;
 	}
-	return (length);
+
+	*i = curr_i - 1;
+
+	return (precision);
+}
+
+#include "main.h"
+
+/**
+ * cyn_size -Calculates the size to cast the argument
+ * @fstr: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
+ *
+ * Return: Precision.
+ */
+int cyn_size(const char *fstr, int *i)
+{
+	int curr_i = *i + 1;
+	int size = 0;
+
+	if (fstr[curr_i] == 'l')
+		size = C_LONG;
+	else if (fstr[curr_i] == 'h')
+		size = C_SHORT;
+
+	if (size == 0)
+		*i = curr_i - 1;
+	else
+		*i = curr_i;
+
+	return (size);
+}
+
+#include "main.h"
+
+/**
+ * cyn_width - Calculates the width for printing
+ * @fstr: Formatted string in which to print the arguments.
+ * @i: List of arguments to be printed.
+ * @val: list of arguments.
+ *
+ * Return: width.
+ */
+int cyn_width(const char *fstr, int *i, va_list val)
+{
+	int curr_i;
+	int width = 0;
+
+	for (curr_i = *i + 1; fstr[curr_i] != '\0'; curr_i++)
+	{
+		if (is_digit(fstr[curr_i]))
+		{
+			width *= 10;
+			width += fstr[curr_i] - '0';
+		}
+		else if (fstr[curr_i] == '*')
+		{
+			curr_i++;
+			width = va_arg(val, int);
+			break;
+		}
+		else
+			break;
+	}
+
+	*i = curr_i - 1;
+
+	return (width);
 }
